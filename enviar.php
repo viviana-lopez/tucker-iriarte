93f359543fd51627fede609e73fedc9dee1c194e
viviana <lilith37.vl@gmail.com>
2021-06-16 00:11:30 -0300

    nuevo php email

--- enviar.php
+++ enviar.php
@@ -1,18 +1,41 @@
 <?php
-$destino = "lilith37.vl@gmail.com";
 
-$nombre = $_POST["nombre"];
-$correo = $_POST["correo"];
-$telefono = $_POST["telefono"];
-$mensaje = $_POST["mensaje"];
+include "class.phpmailer.php";
+include "class.smtp.php";
 
-$contenido = "Nombre: ". $nombre . 
-            "\nCorreo: " . $correo . 
-            "\nTelefono: " . $telefono . 
-            "\nMensaje: " . $mensaje;
+$email_user = "lilith37.vl";
+$email_password = "gato0123";
+$the_subject = "Email de prueba a tu casilla";
+$address_to = "ale.eu.rod@gmail.com";
+$from_name = "Viviana";
+$phpmailer = new PHPMailer();
 
-mail($destino,"Contacto", $contenido);
+// ———- datos de la cuenta de Gmail ——————————-
+$phpmailer->Username = $email_user;
+$phpmailer->Password = $email_password; 
+//———————————————————————–
+// $phpmailer->SMTPDebug = 1;
+$phpmailer->SMTPSecure = 'ssl';
+$phpmailer->Host = "smtp.gmail.com"; // GMail
+$phpmailer->Port = 465;
+$phpmailer->IsSMTP(); // use SMTP
+$phpmailer->SMTPAuth = true;
 
-header("location:gracias.html");
+$phpmailer->setFrom($phpmailer->Username,$from_name);
+$phpmailer->AddAddress($address_to); // recipients email
 
+$phpmailer->Subject = $the_subject;	
+$phpmailer->Body .="<h1 style='color:#3498db;'>Hola Mundo! soy viviana mandando un correo electronico desde una página</h1>";
+$phpmailer->Body .= "<p>Mensaje personalizado de vivi</p>";
+$phpmailer->Body .= "<p>Fecha y Hora: ".date("d-m-Y h:i:s")."</p>";
+$phpmailer->IsHTML(true);
+
+$phpmailer->Send();
 ?>
+
+
+
+
+
+
+
